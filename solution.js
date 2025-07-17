a2433a1ea9603faf1b0d6412ebadc4565f20e9fd
Monikajami <jvrjvr747@gmail.com>
2025-07-17 17:30:44 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -6,12 +6,12 @@ const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "school",
-  password: "123456",
-  port: 5432,
-});
+  user: ****,
+  host: ****,
+  database: ****,
+  password: ****,
+  port: ****,
+})
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
